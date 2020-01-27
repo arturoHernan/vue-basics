177f05102343793e214cbddbcc01f8ab9613d371
Arturo <arturohdezl85@gmail.com>
2020-01-26 23:43:46 -0600

    Data & Methods

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Arturo'
+    },
+    methods: {
+        sayHello: function() {
+            return "hello " + this.name
+        }
     }
 });
